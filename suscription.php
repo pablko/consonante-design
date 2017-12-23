717f280f7977e06f2c918fa938f38968afd5f7d2
Pablko <pablo.colqui@gmail.com>
2017-12-23 11:31:32 -0300

    Agregando 'gracias.html'

--- suscription.php
+++ suscription.php
@@ -3,7 +3,32 @@
     $nombre = $_POST["nombre"];
     $correo = $_POST["correo"];
     $telefono = $_POST["telefono"];
-    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo .  "n\Teléfono: " . $telefono;
-    mail($destino,"Contacto de CSNT", $contenido);
+    $contenido = 
+        "
+        <html lang='es'>
+        <head>
+            <title>Contacto de CSNT Design</title>
+            <meta charset='UTF-8'>
+            <link rel='stylesheet' href='css/bootstrap.min.css'>
+            <link rel='stylesheet' href='css/mail.css'>
+        </head>
+        <body>
+            <div class='container' style='background-color: #bed4de'>
+                <h1>Nuevo contacto de CSNT Design</h1>
+                <table>
+                    <tr>
+                      <th>Nombre</th><th>Correo</th><th>Teléfono</th>
+                    </tr>
+                    <tr>
+                      <td>" . $nombre . "</td><td>" . $correo . "</td><td>" . $telefono . " </td>
+                    </tr>
+                </table>
+            </div>
+        </body>
+        </html>
+        ";
+    $cabeceras  = 'MIME-Version: 1.0' . "\n";
+    $cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\n";
+    mail($destino,"Contacto de CSNT", $contenido, $cabeceras);
     header("Location:gracias.html");
 ?>
